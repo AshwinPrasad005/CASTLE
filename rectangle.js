bc8d64b81a1dc54a4378df7e3f879b646bb1ca4d
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-05-07 19:12:50 +0530

    Add files via upload

---
+++ rectangle.js
@@ -0,0 +1,27 @@
+class Rectangle{
+
+    constructor(x,y,witdh,height,angle){
+        var option = {
+            isStatic = true
+        }
+
+        this.body = Bodies.rectangle(x,y.witdh,heiht,angle);
+        this.height = height;
+        this.witdh = witdh;
+        World.add(world,this.body);
+
+    }
+
+     display(){
+
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rectMode(CENTER);
+        rect(this.body,0,0,this.witdh,this.height);
+
+    }
+    
+    
+
+}
---
+++ sketch.js
@@ -0,0 +1,26 @@
+var ground;
+
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+const World = Matter.World;
+const Body = Matter.World;
+const Render = Matter.World;
+
+function setup() {
+  createCanvas(600,600);
+
+  ground = createSprite(200,375,400,50);
+  
+  var box1 = new Rectangle(200,300,50,100);
+
+  engine = Engine.create();
+  world = engine.world;
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+
+  box1.display();
+
+}
