b1bdce8cdfca628a99a849ebea10b4cefd532f21
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-05-22 11:15:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,9 @@
-var ground,world,engine,box1;
-
-const Engine = Matter.Engine;
-const Bodies = Matter.Bodies;
-const World = Matter.World;
-const Body = Matter.World;
-
 function setup() {
-  var canvas = createCanvas(600,600);
-
-  background("orange");
-
-  ground = createSprite(200,375,400,50);
-
-  engine = Engine.create();
-  world = engine.world;
-
-  box1 = new Rectangle(200,300,50,100);
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
   background(255,255,255);  
   drawSprites();
-
-  box1.display();
-
 }
